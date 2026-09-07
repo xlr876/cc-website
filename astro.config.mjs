1ad1059796d860a7b17ba620df36bce845aedb1d
Santiago Sosa <santiaghox@hotmail.com>
2026-09-06 22:54:01 -0400

    Create shell website

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,13 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://YOUR-GITHUB-USERNAME.github.io",
+  base: "/cc-website",
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
